94f0f4606b76488285e0525b6b2935a4a0ea9269
Kevin Vu <hongkhanhvu94@gmail.com>
2023-07-09 14:46:52 +1000

    finish the game play in console

--- console/main-console.js
+++ console/main-console.js
@@ -2,18 +2,18 @@ const choices = ["rock", "paper", "scissors"];
 const winners = [];
 
 function game() {
-  for (let i = 0; i < 5; i++) {
-    playRound();
+  for (let i = 1; i <= 5; i++) {
+    playRound(i);
   }
   logWins();
 }
 
-function playRound() {
+function playRound(round) {
   const playerSelection = playerChoice();
   const computerSelection = computerChoice();
-  console.log(computerSelection);
   const winner = checkWinner(playerSelection, computerSelection);
   winners.push(winner);
+  logRound(playerSelection, computerSelection, winner, round);
 }
 
 function playerChoice() {
@@ -56,7 +56,19 @@ function checkWinner(choiceP, choiceC) {
 }
 
 function logWins() {
-  console.log(winners);
+  let playerWins = winners.filter((item) => item == "Player").length;
+  let computerWins = winners.filter((item) => item == "Computer").length;
+  let ties = winners.filter((item) => item == "Tie").length;
+  console.log("Results:");
+  console.log("Player Wins: ", playerWins);
+  console.log("Computer Wins: ", computerWins);
+  console.log("Ties: ", ties);
 }
 
-game();
+function logRound(playerChoice, computerChoice, winner, round) {
+  console.log("Round: ", round);
+  console.log("Player chose: ", playerChoice);
+  console.log("Computer chose: ", computerChoice);
+  console.log(winner, " won the round");
+  console.log("-----------------------------");
+}
